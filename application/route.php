b7a6cb3125f812ffba45050ea940a0b3166033a7
localpeople <695152458@qq.com>
2018-01-18 19:56:02 +0800

    3-6获取请求参数

--- application/route.php
+++ application/route.php
@@ -9,13 +9,20 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 
-return [
-    '__pattern__' => [
-        'name' => '\w+',
-    ],
-    '[hello]'     => [
-        ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
-        ':name' => ['index/hello', ['method' => 'post']],
-    ],
+//return [
+//    '__pattern__' => [
+//        'name' => '\w+',
+//    ],
+//    '[hello]'     => [
+//        ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
+//        ':name' => ['index/hello', ['method' => 'post']],
+//    ],
+//
+//];
+use think\Route;
 
-];
+// tp5路由功能（开启会屏蔽相应PATH_INFO路径）
+// Route::rule('路由表达式','路由地址','请求类型','路由参数（数组）','变量规则（数组）');
+//Route::rule('hello/:id/:name', 'sample/test/hello', 'GET|POST', ['https'=>false]);
+//Route::get('hello', 'sample/test/hello');
+Route::post('hello/:id', 'sample/test/hello');
--- application/sample/controller/Test.php
+++ application/sample/controller/Test.php
@@ -9,9 +9,20 @@
 namespace app\sample\controller;
 
 
+use think\Request;
+
 class Test
 {
-    public function hello(){
-        return 'hello, diamon';
+    public function hello(/*$id, $name, $age*/){
+        $id = Request::instance()->param('id');
+        $name = Request::instance()->param('name');
+        $age = Request::instance()->param('age');
+
+        echo $id;
+        echo '|';
+        echo $name;
+        echo '|';
+        echo $age;
+//        return 'hello, diamon';
     }
 }
