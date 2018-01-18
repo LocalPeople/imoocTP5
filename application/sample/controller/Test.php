00b2b2671137bdf56361160d34649c237e7ef532
localpeople <695152458@qq.com>
2018-01-18 17:04:13 +0800

    3-2配置虚拟域名简化URL路径

---
+++ application/sample/controller/Test.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Wen
+ * Date: 2018/1/18
+ * Time: 16:33
+ */
+
+namespace app\sample\controller;
+
+
+class Test
+{
+    public function hello(){
+        return 'hello, diamon';
+    }
+}
---
+++ public/info.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Wen
+ * Date: 2018/1/18
+ * Time: 15:28
+ */
+
+phpinfo();
